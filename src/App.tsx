c0c073b5094cbc45bf487b900464ce52c5cdbc03
nathalie <103376453+nathalie7890@users.noreply.github.com>
2023-08-22 19:42:54 +0800

    refactor some code

--- src/App.tsx
+++ src/App.tsx
@@ -13,87 +13,125 @@ import { AiOutlineMail, AiFillGithub } from "react-icons/ai";
 import Contact from "./components/contact/contact";
 
 function App() {
+  const renderContainer = (
+    leftContent: React.ReactNode,
+    rightContent: React.ReactNode,
+    extraContainerClassName?: string,
+    leftContainerExtraClassName?: string,
+    rightContainerExtraClassname?: string
+  ) => {
+    const containerClassName = `flex lg:flex-row flex-col ${extraContainerClassName}`;
+    const leftContainerClassName = `lg:w-1/2 flex flex-col justify-center items-end p-12 text-dark gap-2 font-merriweather bg-light  ${leftContainerExtraClassName}`;
+    const rightContainerClassName = `lg:w-1/2 flex flex-col justify-center items-start p-12 text-light gap-2 font-montserrat bg-dark ${rightContainerExtraClassname}`;
+    return (
+      <div className={containerClassName}>
+        <div className={leftContainerClassName}>{leftContent}</div>
+        <div className={rightContainerClassName}>{rightContent}</div>
+      </div>
+    );
+  };
+
+  const renderAbout = () => {
+    const extraContainerClassName = "md:h-screen";
+    const leftContainerClassName = "pt-36 lg:pt-0";
+
+    const leftContent = renderTitle("nathalie teh", 'web developer');
+    const rightContent = (
+      <h1 className="font-montserrat text-sm sm:text-base">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        eleifend porta odio in lacinia. Vivamus eleifend ornare ante, ut
+        ultrices nibh laoreet in. Quisque luctus ligula dui, et pellentesque
+        odio interdum rutrum. Donec vitae accumsan magna. Cras hendrerit, augue
+        vel sagittis interdum, nisl diam efficitur velit, id varius libero sem
+        sed risus.
+      </h1>
+    );
+
+    return renderContainer(
+      leftContent,
+      rightContent,
+      extraContainerClassName,
+      leftContainerClassName
+    );
+  };
+
+  const renderSkills = () => {
+    const leftContent = renderTitle("skills");
+    const rightContent = (
+      <div className="gap-6 flex flex-col">
+        <h1 className="font-montserrat text-sm sm:text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          eleifend porta odio in lacinia. Vivamus eleifend ornare ante, ut
+          ultrices nibh laoreet in. Quisque luctus ligula dui, et pellentesque
+          odio interdum rutrum.
+        </h1>
+        <div className="flex items-center gap-1.5">
+          <BiLogoReact size={25} />
+          <TbBrandNextjs size={25} />
+          <BiLogoTailwindCss size={25} />
+          <DiJavascript1 size={25} />
+          <BiLogoTypescript size={25} />
+          <BiLogoPython size={25} />
+          <SiKotlin size={17} />
+          <BiLogoFlutter size={23} />
+        </div>
+      </div>
+    );
+
+    return renderContainer(leftContent, rightContent);
+  };
+
+  const renderContact = () => {
+    const leftContent = renderTitle("contact");
+    const rightContent = (
+      <div className="flex flex-col w-full">
+        <div className="flex flex-col gap-2 mt-6">
+          <div className="flex gap-2 items-center">
+            <AiOutlineMail size={25} />
+            <p>londonhippos@gmail.com</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <AiFillGithub size={25} />
+            <p>https://github.com/nathalie7890</p>
+          </div>
+        </div>
+        <div className="mt-10 w-full">
+          <h1 className="mb-6 text-2xl">Leave me a message</h1>
+          <Contact />
+        </div>
+      </div>
+    );
+
+    return renderContainer(leftContent, rightContent);
+  };
+
+  const renderTitle = (title: string, subtitle?: string) => {
+    return (
+      <>
+        <h1 className="lg:text-7xl md:text-6xl text-5xl font-semibold">
+          {title}
+        </h1>
+        <h3 className="text-xl mt-2 font-semibold">{subtitle}</h3>
+      </>
+    );
+  };
+
   return (
     <div className="relative">
-      <div className="fixed top-0 right-0 flex justify-end text-light/50 py-4 px-10 gap-6 font-montserrat bg-dark w-1/2">
+      <div className="fixed top-0 right-0 flex justify-end text-light/50 py-4 px-10 gap-6 font-montserrat bg-dark lg:w-1/2 w-full">
         <h1>About</h1>
         <h1>Portfolio</h1>
         <h1>Contact</h1>
       </div>
-      {/* FIRST PAGE */}
-      <div className="flex h-screen">
-        <div className="w-full h-full flex flex-col justify-center items-end p-12 text-dark gap-2 font-merriweather bg-light">
-          <h1 className="text-7xl font-semibold">nathalie</h1>
-          <h1 className="text-xl">I want to be rich</h1>
+      {renderAbout()}
+      {renderSkills()}
+      <div className="flex lg:flex-row flex-col">
+        <div className="lg:w-1/2 flex flex-col justify-center items-end p-12 text-dark gap-2 font-merriweather bg-light">
+          {renderTitle("portfolio")}
         </div>
-        <div className="w-full h-full flex flex-col justify-center items-center p-12 bg-dark text-light">
-          <h1 className="font-montserrat">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            eleifend porta odio in lacinia. Vivamus eleifend ornare ante, ut
-            ultrices nibh laoreet in. Quisque luctus ligula dui, et pellentesque
-            odio interdum rutrum. Donec vitae accumsan magna. Cras hendrerit,
-            augue vel sagittis interdum, nisl diam efficitur velit, id varius
-            libero sem sed risus.
-          </h1>
-        </div>
+        <div className="lg:w-1/2 flex flex-col justify-center items-start p-12 text-light gap-2 font-montserrat bg-dark"></div>
       </div>
-      {/* SECOND PAGE */}
-      <div className="h-fit flex">
-        <div className="w-full flex flex-col justify-center items-end p-12 text-dark gap-2 font-merriweather bg-light">
-          <h1 className="text-7xl font-semibold">skills</h1>
-        </div>
-        <div className="w-full  flex flex-col justify-center items-center p-12 text-light gap-2 font-montserrat bg-dark">
-          <div className="gap-6 flex flex-col">
-            <h1 className="font-montserrat">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              eleifend porta odio in lacinia. Vivamus eleifend ornare ante, ut
-              ultrices nibh laoreet in. Quisque luctus ligula dui, et
-              pellentesque odio interdum rutrum.
-            </h1>
-            <div className="flex items-center gap-1.5">
-              <BiLogoReact size={25} />
-              <TbBrandNextjs size={25} />
-              <BiLogoTailwindCss size={25} />
-              <DiJavascript1 size={25} />
-              <BiLogoTypescript size={25} />
-              <BiLogoPython size={25} />
-              <SiKotlin size={17} />
-              <BiLogoFlutter size={23} />
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* THIRD PAGE */}
-      <div className="h-fit flex">
-        <div className="w-full flex flex-col justify-center items-end p-12 text-dark gap-2 font-merriweather bg-light">
-          <h1 className="text-7xl font-semibold">portfolio</h1>
-        </div>
-        <div className="w-full  flex flex-col justify-center items-start p-12 text-light gap-2 font-montserrat bg-dark"></div>
-      </div>
-      {/* FOURTH PAGE */}
-      <div className="h-fit flex w-full">
-        <div className="w-full flex flex-col justify-center items-end p-12 text-dark gap-2 font-merriweather bg-light">
-          <h1 className="text-7xl font-semibold">contact</h1>
-        </div>
-        <div className="w-full flex flex-col justify-center items-start p-12 text-light gap-2 font-montserrat bg-dark">
-          <div className="flex flex-col w-full">
-            <div className="flex flex-col gap-2 mt-6">
-              <div className="flex gap-2 items-center">
-                <AiOutlineMail size={25} />
-                <p>londonhippos@gmail.com</p>
-              </div>
-              <div className="flex gap-2 items-center">
-                <AiFillGithub size={25} />
-                <p>https://github.com/nathalie7890</p>
-              </div>
-            </div>
-            <div className="mt-10 w-full">
-              <h1 className="mb-6 text-2xl">Leave me a message</h1>
-              <Contact />
-            </div>
-          </div>
-        </div>
-      </div>
+      {renderContact()}
     </div>
   );
 }
--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,25 +1,47 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
-  const contactOnSubmit = (e) => {
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent) => {
     e.preventDefault();
+
+    emailjs
+      .sendForm(
+        `${import.meta.env.VITE_SERVICE_ID}`,
+        `${import.meta.env.VITE_TEMPLATE_ID}`,
+        formRef.current as HTMLFormElement,
+        `${import.meta.env.VITE_PUBLIC_KEY}`
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   };
 
   return (
     <form
-      onSubmit={contactOnSubmit}
+      ref={formRef}
+      onSubmit={sendEmail}
       className="flex flex-col w-full text-light gap-4"
     >
-      <div className="flex gap-4 w-full">
+      <div className="flex gap-4">
         <input
           type="text"
           name="name"
           placeholder="Name"
-          className="border border-light rounded-md bg-dark p-2 placeholder:text-light/50 focus:ring-light focus:ring-1 focus:outline-none flex-grow"
+          className="border border-light rounded-md bg-dark p-2 placeholder:text-light/50 focus:ring-light focus:ring-1 focus:outline-none w-1/2"
         />
         <input
           type="text"
           name="email"
           placeholder="Email"
-          className="border border-light rounded-md bg-dark p-2 placeholder:text-light/50 focus:ring-light focus:ring-1 focus:outline-none flex-grow"
+          className="border border-light rounded-md bg-dark p-2 placeholder:text-light/50 focus:ring-light focus:ring-1 focus:outline-none w-1/2"
         />
       </div>
       <input
